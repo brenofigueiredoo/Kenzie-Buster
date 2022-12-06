2c1fa2bb9e660a820861636542a58f057394ef0d
unknown <brenos93@gmail.com>
2022-12-06 09:20:20 -0300

    feat: tarefa 1

---
+++ users/serializer.py
@@ -0,0 +1,36 @@
+from rest_framework import serializers
+from .models import User
+from rest_framework.validators import UniqueValidator
+
+
+class UserSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    username = serializers.CharField(
+        max_length=20,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(), message="username already taken."
+            )
+        ],
+    )
+    email = serializers.CharField(
+        max_length=127,
+        validators=[
+            UniqueValidator(
+                queryset=User.objects.all(), message="email already registered."
+            )
+        ],
+    )
+    birthdate = serializers.DateField(required=False)
+    first_name = serializers.CharField(max_length=50)
+    last_name = serializers.CharField(max_length=50)
+    password = serializers.CharField(write_only=True)
+    is_employee = serializers.BooleanField(default=False)
+    is_superuser = serializers.BooleanField(read_only=True)
+
+    def create(self, validated_data):
+        is_employee = validated_data["is_employee"]
+        if is_employee:
+            return User.objects.create_user(is_superuser=True, **validated_data)
+        else:
+            return User.objects.create_user(is_superuser=False, **validated_data)
